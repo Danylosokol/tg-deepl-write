dee4c7cf0f7234d9c690433ec02367cfaaa1d40c
dsoSU <dso@scaleup.agency>
2023-08-30 18:41:58 +0100

    test with browserless

--- src/text/deepl.ts
+++ src/text/deepl.ts
@@ -5,8 +5,6 @@ const replyToMessage = async (ctx: any, messageId: string, string: string) =>
 		reply_to_message_id: messageId,
 	});
 
-  
-// trigger deploy 1.1
 const deepl = async () => async (ctx: any) => {
 	const messageId = ctx.message.message_id;
   await replyToMessage(ctx, messageId, `I am improving your text, please wait...`);
